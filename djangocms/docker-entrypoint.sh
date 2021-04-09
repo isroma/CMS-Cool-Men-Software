e59eb7a41834587f8fddb058eb7550e97e73444d
rodmarzz <ismael.rodriguez.practicas@telefonica.com>
2021-04-09 02:46:27 +0200

    Create entrypoint

---
+++ djangocms/docker-entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Create superuser if it doesnt exists on database
+echo "----- Create superuser if necessary -----"
+python manage.py initadmin --username=admin --email=admin@example.com --password=admin 
+
+# Make database migrations before applying
+echo "----- Make database migrations -----"
+python manage.py makemigrations
+
+# Apply database migrations
+echo "----- Apply database migrations -----"
+python manage.py migrate
+
+# Start server
+echo "----- Starting server on port 5432 -----"
+python manage.py runserver 0.0.0.0:5432
