745327bafcd5c8a49679dc09b1722a5f07f305f2
rodmarzz <ismael.rodriguez.practicas@telefonica.com>
2021-04-10 23:48:22 +0200

    Custom filter and list display for future needs

--- djangocms/users/models.py
+++ djangocms/users/models.py
@@ -13,4 +13,4 @@ class Profile(models.Model):
     verified = models.BooleanField(default=False)
 
     def __str__(self):
-        return self.user.username
+        return f"{self.user}, {self.verified}"
