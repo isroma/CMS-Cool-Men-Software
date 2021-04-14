cd77a696edb5865cc021ba642d1037c72cdec950
rodmarzz <ismael.rodriguez.practicas@telefonica.com>
2021-04-14 18:31:59 +0200

    Setting up SwiftStack connection

--- djangocms/homepage/views.py
+++ djangocms/homepage/views.py
@@ -6,6 +6,9 @@ import random
 import string
 import time
 import uuid
+import re
+
+# TODO: clean trash
 
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
@@ -14,11 +17,8 @@ from django.urls import reverse
 from django.template import RequestContext
 
 from urllib.parse import urlparse
-
 from swiftclient import client
-
 from tika import parser
-
 from .models import StorageObject
 
 def index(request):
@@ -76,6 +76,7 @@ def download(request, pk):
     so = StorageObject.objects.get(pk=pk)
 
     storage_url, key = get_tempurl_key()
+    storage_url = "http://0.0.0.0:8080/v1/AUTH_test"
     url = "%s/%s/%s" % (storage_url, so.container, so.objectname)
 
     expires = int(time.time() + 60)
@@ -83,14 +84,15 @@ def download(request, pk):
 
     hmac_body = 'GET\n%s\n%s' % (expires, path)
     signature = hmac.new(bytearray(key.encode('utf-8')), hmac_body.encode('utf-8'), hashlib.sha1).hexdigest()
-    signed_url = '%s?temp_url_sig=%s&temp_url_expires=%s' % (
-        url, signature, expires)
+    signed_url = '%s?temp_url_sig=%s&temp_url_expires=%s' % (url, signature, expires)
 
     return redirect(signed_url)
 
 
 def upload(request):
     storage_url, key = get_tempurl_key()
+    # TODO: this can be done better
+    storage_url = "http://0.0.0.0:8080/v1/AUTH_test"
     prefix = str(uuid.uuid4())
 
     # In a real-world scenario you might want to record the prefix in a DB
@@ -104,33 +106,28 @@ def upload(request):
     max_file_count = 1
     expires = int(time.time() + 5*60)
     swift_url = "%s/%s/%s/" % (storage_url, settings.SWIFT_CONTAINER, prefix)
-    redirect_url = "http://%s%s" % (
-        request.get_host(), reverse(finalize, kwargs={'prefix': prefix}))
+    redirect_url = "http://%s%s" % (request.get_host(), reverse(finalize, kwargs={'prefix': prefix}))
     path = urlparse(swift_url).path
-    print(path)
 
-    hmac_body = '%s\n%s\n%s\n%s\n%s' % (
-        path, redirect_url, max_file_size, max_file_count, expires)
+    hmac_body = '%s\n%s\n%s\n%s\n%s' % (path, redirect_url, max_file_size, max_file_count, expires)
     signature = hmac.new(bytearray(key.encode('utf-8')), hmac_body.encode('utf-8'), hashlib.sha1).hexdigest()
 
     context = {
         'swift_url': swift_url, 'redirect_url': redirect_url,
         'max_file_size': max_file_size, 'max_file_count': max_file_count,
         'expires': expires, 'signature': signature
-        }
+    }
 
     return render(request, 'upload.html', context)
 
 
 def finalize(request, prefix):
-    (storage_url, auth_token) = client.get_auth(
-        settings.SWIFT_AUTH_URL, settings.SWIFT_USER, settings.SWIFT_PASSWORD)
+    (storage_url, auth_token) = client.get_auth(settings.SWIFT_AUTH_URL, settings.SWIFT_USER, settings.SWIFT_PASSWORD)
 
     # Note: uploaded objects might not be listed yet due to the eventual
     # consistency. You might want to run an async job after some time to find
     # objects that are already uploaded, but not yet referenced in the DB.
-    _meta, objects = client.get_container(
-        storage_url, auth_token, settings.SWIFT_CONTAINER, prefix=prefix)
+    _meta, objects = client.get_container(storage_url, auth_token, settings.SWIFT_CONTAINER, prefix=prefix)
 
     # Remember DB ids. These are guessable, thus a real world app should use a
     # more sophisticated approach
@@ -141,6 +138,6 @@ def finalize(request, prefix):
             objectname=obj.get('name'),
             prefix=prefix)
         dbentry.save()
-        ids.append(dbentry.id - 1)
+        ids.append(dbentry.id)
 
     return render(request, 'finalize.html', {'ids': ids, 'host': request.get_host()})
