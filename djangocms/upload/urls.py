4ec32a6ae658093a0988b39682f1e7714a9d2bea
rodmarzz <ismael.rodriguez.practicas@telefonica.com>
2021-04-30 21:03:43 +0200

    Comment unused URLs

--- djangocms/upload/urls.py
+++ djangocms/upload/urls.py
@@ -2,10 +2,10 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('upload', views.upload, name='upload'),
-    path('finalize', views.finalize, name='finalize'),
-    path('random_url', views.tika, name='random_url'),
+    # path('upload', views.upload, name='upload'),
+    # path('finalize', views.finalize, name='finalize'),
+    # path('random_url', views.tika, name='random_url'),
     re_path(r'^d/(?P<pk>.+?)$', views.download, name="download"),
     re_path(r'^finalize/(?P<prefix>.*?)$', views.finalize, name="finalize"),
-    re_path(r'^$', views.upload, name="upload"),
+    re_path(r'^upload/$', views.upload, name="upload"),
 ]
