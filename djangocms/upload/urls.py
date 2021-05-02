f94682e927301ad809204fbfa5143d206613730a
rodmarzz <ismael.rodriguez.practicas@telefonica.com>
2021-05-02 20:55:23 +0200

    Updating tika examples

--- djangocms/upload/urls.py
+++ djangocms/upload/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     # path('upload', views.upload, name='upload'),
     # path('finalize', views.finalize, name='finalize'),
-    path('upload/random_url/', views.tika, name='random_url'),
+    path('upload/tika/', views.tika, name='tika'),
     re_path(r'^d/(?P<pk>.+?)$', views.download, name="download"),
     re_path(r'^finalize/(?P<prefix>.*?)$', views.finalize, name="finalize"),
     re_path(r'^upload/$', views.upload, name="upload"),
