e40a1711cff8dc3d9cf53064d6443d63ed344500
rodmarzz <ismael.rodriguez.practicas@telefonica.com>
2021-05-09 23:30:56 +0200

    ElasticSearch search now depends on user's roles

--- djangocms/search/views.py
+++ djangocms/search/views.py
@@ -3,7 +3,7 @@ from django_elasticsearch_dsl import Document, Date, Integer, Keyword, Text
 from django_elasticsearch_dsl.registries import registry
 from elasticsearch_dsl.query import MultiMatch
 from search.models import Post
-from users.models import Role
+from users.models import Role, Profile
 
 # Create your views here.
 roles = Role.objects.all()
@@ -36,9 +36,11 @@ def search(request):
         mq = MultiMatch(query=q, fields=['titulo', 'descripcion'], fuzziness='AUTO')
         posts = PostDocument.search(index='cms').query(mq)
 
+    user = Profile.objects.get(user=request.user)
+
     context = {
         'posts': posts,
-        'roles': roles,
+        'roles': user.roles.all(),
         'id': id
     }
 
