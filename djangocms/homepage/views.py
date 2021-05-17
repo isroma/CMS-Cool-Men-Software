50846cc04bb8aea08cb974036751694c5ebc2f0b
Yostita <jlbellostamanchon@gmail.com>
2021-05-17 22:53:14 +0200

    Revert "feature(index): merge search in index [CMS-78]"
    This reverts commit aabe35231e957d38e3407bda210acfa09f41759d.

--- djangocms/homepage/views.py
+++ djangocms/homepage/views.py
@@ -1,12 +1,4 @@
 from django.shortcuts import render
-from users.models import Role, Profile
 
-def search(request):
-    user = Profile.objects.get(user=request.user)
-    roles = Role.objects.all()
-
-    context = {
-        'roles': user.roles.all()
-    }
-
-    return render(request, 'index.html', context)
+def index(request):
+    return render(request, 'index.html')
