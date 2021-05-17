27b82efae5567943559ed9387f4cb99a3809d2ee
Yostita <jlbellostamanchon@gmail.com>
2021-05-17 23:02:26 +0200

    feature(index): migrate searh with index

--- djangocms/homepage/views.py
+++ djangocms/homepage/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from users.models import Role, Profile
 
-def index(request):
-    return render(request, 'index.html')
+# Create your views here.
+
+def search(request):
+    user = Profile.objects.get(user=request.user)
+    roles = Role.objects.all()
+
+    context = {
+        'roles': user.roles.all()
+    }
+
+    return render(request, 'index.html', context)
